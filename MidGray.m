773e0169914cbbd438a34fae9767018071d1b569
Liming Cai <daybreak.chua@gmail.com>
2022-11-21 14:34:48 -0600

    Create MidGray.m

---
+++ MidGray.m
@@ -0,0 +1,11 @@
+function
+x=imread();
+[r,g,b]=imsplit(x)
+se=strel("disk",2);
+lmin=imerode(g,se);
+lmax=imdilate(g,se);
+mg=(lmin+lmax)/2;
+mg=imadjust(mg);
+z=imbinarize(mg);
+z=~z;
+sk=bwmorph(z,"thin",inf);
