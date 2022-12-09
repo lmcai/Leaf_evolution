5a621666b466359d9085742f0afd6d7bb107fe79
Liming Cai <daybreak.chua@gmail.com>
2022-12-09 15:23:12 -0600

    Update MidGray.m

--- MidGray.m
+++ MidGray.m
@@ -1,7 +1,8 @@
 function
 x=imread();
-[r,g,b]=imsplit(x)
+[r,g,b]=imsplit(x);
 se=strel("disk",2);
+g=adapthisteq(g,'NumTiles',[15 15],'ClipLimit',0.02);
 lmin=imerode(g,se);
 lmax=imdilate(g,se);
 mg=(lmin+lmax)/2;
