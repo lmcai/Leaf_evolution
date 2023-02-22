a414a287f2bfacfc2636eb2a31711b032ec3ddfb
Liming Cai <daybreak.chua@gmail.com>
2023-02-22 10:25:13 -0600

    updates

---
+++ remove_white_background_chatGPT.m
@@ -0,0 +1,26 @@
+% Load the PNG image
+img = imread('image.png');
+
+% Convert the image to grayscale
+grayImg = rgb2gray(img);
+
+% Threshold the image to create a binary mask
+mask = grayImg < 220; % adjust threshold value as needed
+
+% Fill holes in the mask
+filledMask = imfill(mask, 'holes');
+
+% Remove small objects from the mask
+filteredMask = bwareaopen(filledMask, 50); % adjust minimum area as needed
+
+% Apply the mask to the original image
+maskedImg = bsxfun(@times, img, cast(filteredMask, 'like', img));
+
+% Display the original and masked images side by side
+figure;
+subplot(1,2,1);
+imshow(img);
+title('Original Image');
+subplot(1,2,2);
+imshow(maskedImg);
+title('Masked Image');
