6558154455a11bbd8f0a3f496b37c7b239607c80
Liming Cai <daybreak.chua@gmail.com>
2023-02-23 17:04:35 -0600

    Update EFD_main.m

--- EFD/EFD_main.m
+++ EFD/EFD_main.m
@@ -2,14 +2,26 @@
 %Segment the image
 imwrite(im2bw(imread('cat.png')), 'bw_cat.png')
 
+% set the number of harmonics
+n=10
+
 % trace exterior boundaries
 chain = mk_chain('bw_cat.png');
 [cc] = chaincode(chain);
 
-%generate EFD
-plot_chain_code(transpose([cc.code]));
-hold
-plot_fourier_approx(transpose([cc.code]), 10, 1000, 0, 'r');
+%output first to n-th EFD coefficients
+for i = 1 : n
+	harmonic_coeff = calc_harmonic_coefficients(transpose([cc.code]), i);
+        a(i) = harmonic_coeff(1, 1);
+        b(i) = harmonic_coeff(1, 2);
+        c(i) = harmonic_coeff(1, 3);
+        d(i) = harmonic_coeff(1, 4);
+end
+
+% plot and compare EFD
+% plot_chain_code(transpose([cc.code]));
+% hold
+% plot_fourier_approx(transpose([cc.code]), n, 1000, 0, 'r');
 
 
 
