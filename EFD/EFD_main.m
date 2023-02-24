27eddb3cf0b0a611936f0c61248f3934300cde6e
Liming Cai <daybreak.chua@gmail.com>
2023-02-24 11:18:15 -0600

    Update EFD_main.m

--- EFD/EFD_main.m
+++ EFD/EFD_main.m
@@ -3,23 +3,25 @@
 % set the number of harmonics
 n=10
 
-% trace exterior boundaries
+%resize the original black-white binary mask of leaf shape occording to the scale
+
+% 'bw.png' is a black-white binary mask of leaf shape, trace exterior boundaries
 chain = mk_chain('bw.png');
 [cc] = chaincode(chain);
 
 %output first to n-th EFD coefficients
 for i = 1 : n
 	harmonic_coeff = calc_harmonic_coefficients(transpose([cc.code]), i);
-        a(i) = harmonic_coeff(1, 1);
-        b(i) = harmonic_coeff(1, 2);
-        c(i) = harmonic_coeff(1, 3);
-        d(i) = harmonic_coeff(1, 4);
+    a(i) = harmonic_coeff(1, 1);
+    b(i) = harmonic_coeff(1, 2);
+    c(i) = harmonic_coeff(1, 3);
+    d(i) = harmonic_coeff(1, 4);
 end
 
 % plot and compare EFD
 % plot_chain_code(transpose([cc.code]));
 % hold
-% plot_fourier_approx(transpose([cc.code]), n, 1000, 0, 'r');
+% plot_fourier_approx(transpose([cc.code]), n, 100, 0, 'r');
 
 
 
