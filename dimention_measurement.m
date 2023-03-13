3bf03fe9c8b7c3ba24bb3632d8722b540a3ac19f
Liming Cai <34404264+lmcai@users.noreply.github.com>
2023-03-13 18:52:10 -0500

    Update dimention_measurement.m

--- dimention_measurement.m
+++ dimention_measurement.m
@@ -1,17 +1,29 @@
 % Load the binary image containing the segregated object
-binary_image = imread('segregated_object.png');
+im = imread('segregated_object.png');
 
 % Calculate the connected components in the binary image
-cc = bwconncomp(binary_image);
+% cc = bwconncomp(im);
 
-% Calculate the properties of the connected components
-props = regionprops(cc, 'BoundingBox', 'Area');
+stats = regionprops(im, 'BoundingBox');
+min_area = Inf;
+for k = 1:length(stats)
+    area = stats(k).BoundingBox(3) * stats(k).BoundingBox(4);
+    if area < min_area
+        min_area = area;
+        min_box = stats(k).BoundingBox;
+        dim1 = stats(k).BoundingBox(3);
+		dim2 = stats(k).BoundingBox(3);
+		width = min([dim1 dim2]);
+		length = max([dim1 dim2]);
+    end
+end
+imshow(im);
+rectangle('Position', min_box, 'EdgeColor', 'r', 'LineWidth', 2);
 
-% Extract the dimensions and area of the first connected component (assuming there's only one)
-bbox = props(1).BoundingBox;
-width = bbox(3);
-height = bbox(4);
-area = props(1).Area;
-
-% Print the dimensions and area
-fprintf('Width: %d\nHeight: %d\nArea: %d\n', width, height, area);
+%rotate Image
+[grad_mag, grad_dir] = imgradient(im);
+[H,theta,rho] = hough(grad_mag);
+peaks = houghpeaks(H, 1);
+angle = theta(peaks(2));
+im_rotated = imrotate(im, -angle, 'bilinear', 'crop');
+imshow(im_rotated);
