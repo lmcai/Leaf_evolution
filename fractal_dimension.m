1f668d3f36eb2da7242334ce072bdfdf4bc736e3
Liming Cai <34404264+lmcai@users.noreply.github.com>
2023-03-27 20:34:43 -0500

    updates

--- fractal_dimension.m
+++ fractal_dimension.m
@@ -1,29 +1,33 @@
-% Load the contour from a file or generate it programmatically
-contour = load('contour.mat'); % replace with your own data
+function fractalDimension = fractal_dimension(contour)
+	% Convert the contour to a binary image
+	binaryImage = imread(contour);
+	
+	% Set the range of grid sizes to use
+	maxBoxSize = min(size(binaryImage));
+	%minBoxSize = fix(maxBoxSize/128);
 
-% Convert the contour to a binary image
-binaryImage = im2bw(contour);
+	% Initialize arrays to store the number of boxes and box sizes
+	numBoxes = zeros(1, 50);
+	boxSizes = zeros(1, 50);
+	
+	% Count the number of boxes that contain any part of the contour for each box size
+	for i = 1:50
+		boxlen=fix(maxBoxSize/i);
+   		boxes = reshape(binaryImage(1:boxlen*floor(size(binaryImage,1)/boxlen),1:boxlen*floor(size(binaryImage,2)/boxlen)), boxlen, boxlen, []);
+    	numBoxes(i) = sum(sum(any(any(boxes))));
+    	boxSizes(i) = i;
+	end
+	%for i = 1:maxBoxSize
+   	%	boxes = reshape(binaryImage(1:i*floor(size(binaryImage,1)/i),1:i*floor(size(binaryImage,2)/i)), i, i, []);
+    %	numBoxes(i) = sum(sum(any(any(boxes))));
+    %	boxSizes(i) = maxBoxSize/i;
+	%end
 
-% Set the range of grid sizes to use
-maxBoxSize = min(size(binaryImage));
-minBoxSize = fix(maxBoxSize/32);
+	% Plot the results on a log-log plot
+	loglog(boxSizes, numBoxes, 'o');
+	xlabel('Box size');
+	ylabel('Number of boxes');
 
-% Initialize arrays to store the number of boxes and box sizes
-numBoxes = zeros(1, maxBoxSize);
-boxSizes = zeros(1, maxBoxSize);
-
-% Count the number of boxes that contain any part of the contour for each box size
-for i = minBoxSize:maxBoxSize
-    boxes = reshape(binaryImage(1:i*floor(size(binaryImage,1)/i),1:i*floor(size(binaryImage,2)/i)), i, i, []);
-    numBoxes(i) = sum(sum(any(any(boxes))));
-    boxSizes(i) = size(boxes,3);
-end
-
-% Plot the results on a log-log plot
-loglog(boxSizes, numBoxes, 'o');
-xlabel('Box size');
-ylabel('Number of boxes');
-
-% Fit a line to the plot and calculate the fractal dimension
-p = polyfit(log(boxSizes), log(numBoxes), 1);
-fractalDimension = -p(1);
+	% Fit a line to the plot and calculate the fractal dimension
+	p = polyfit(log10(boxSizes), log10(numBoxes), 1);
+	fractalDimension = p(1);
