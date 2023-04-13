8cb85439ec7e361fe84f3e97749d00b10256de59
Liming Cai <34404264+lmcai@users.noreply.github.com>
2023-04-12 20:20:56 -0500

    updates

--- 1_EFD/test.m
+++
@@ -1,4 +0,0 @@
-Bound = bwboundaries(im, 8,'noholes');
-outlineLeaf = cell2mat(Bound); %make outline a matrix
-[normalized_outline, ratioScale] = normalize_outline(outlineLeaf);
-[efdOutline, coefficients] = efd_setup(numHarmonics, normalized_outline, ratioScale);
