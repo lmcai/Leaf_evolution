db8296c6876fe83162e88530d14ecff295e5e51f
Liming Cai <34404264+lmcai@users.noreply.github.com>
2025-02-05 12:07:11 -0500

    Update batch_clean_sketchbook_trace.m

--- 3_vein_extraction/batch_clean_sketchbook_trace.m
+++ 3_vein_extraction/batch_clean_sketchbook_trace.m
@@ -2,26 +2,33 @@
 img = imread('your_image.png');
 
 % Filter red and color
-red_indices = img(:,:,1) > 200 & img(:,:,2) < 20 & img(:,:,3) < 20;
+red_indices = img(:,:,1) > 200 & img(:,:,2) < 50 & img(:,:,3) < 50;
 yellow_indices = img(:,:,1) > 220 & img(:,:,2) > 200 & img(:,:,3) < 20;
 
-se = strel('diamond', 3);
-red_mask=imopen(red_indices,se);
+% se = strel('diamond', 3);
+% red_mask=imopen(red_indices,se);
 yellow_mask=imopen(yellow_indices,se);
 yellow_mask=imclose(yellow_indices,se);
 
-red_mask = red_mask > 0;
-yellow_mask = yellow_mask > 0;
+red_mask = red_indices > 0;
+yellow_mask = yellow_indices > 0;
 
 % Create a white background image
 %white_bg = uint8(ones(size(red_mask)) * 255);
 
+% Find the overlapping region and exclude it from the yellow mask
+overlap_region = red_mask & yellow_mask;
+yellow_mask(overlap_region) = 0;
+red_mask(overlap_region) = 1;
+
+% Fill gap with red
+redyellow_mask= red_mask + yellow_mask;
+filled_redyellow_mask=imfill(redyellow_mask,'holes');
+gap=filled_redyellow_mask & ~redyellow_mask;
+red_mask(gap)=1;
+
 % Assign red pixels where red_mask is true
 output_img = cat(3, red_mask * 255, zeros(size(red_mask)), zeros(size(red_mask)));
-
-% Find the overlapping region and exclude it from the yellow mask
-%overlap_region = red_mask & yellow_mask;
-%yellow_mask(overlap_region) = 0;
 
 % Assign yellow pixels where yellow_mask is true and not overlapping with red
 output_img(:,:,1) = output_img(:,:,1) + yellow_mask * 255;
