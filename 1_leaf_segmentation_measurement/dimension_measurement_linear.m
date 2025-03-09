f44b92cfcd244021be4b9fdb8f83e3dc1bf2c049
Liming Cai <34404264+lmcai@users.noreply.github.com>
2025-03-09 03:31:42 -0400

    updates

--- 1_leaf_segmentation_measurement/dimension_measurement_linear.m
+++ 1_leaf_segmentation_measurement/dimension_measurement_linear.m
@@ -1,9 +1,10 @@
 function [width width_bbx length_bbx total_area solidity circularity EI]=dimension_measurement(im)
 	stats = regionprops(im, 'BoundingBox');
 	width = get_width(im);
-	%length_linear = linear_leaf_len(im);
+	skeleton=bwmorph(BW,'thin',5);
+	
+	length_bbx = ;
 	width_bbx = stats(1).BoundingBox(3);
-	length_bbx = stats(1).BoundingBox(4);
 	% Calculate the connected components in the binary image
 	cc = bwconncomp(im);
 	% Calculate the region properties for each connected component
@@ -37,24 +38,3 @@ function [width width_bbx length_bbx total_area solidity circularity EI]=dimensi
 	
 	%EI = 4 A/(πLW)
 	EI = (4*total_area)/(pi*length_bbx*width);
-	
-	%LeafBwStats = regionprops(rotatedLeafBw,'all');
-	%[RDMX,RDMY,RCMX,RCMY,RDNX,RDNY,RCNX,RCNY] = CalcAxis(LeafBwStats);
-	%tabledatarotated = regionprops('Table',rotatedLeafBw, 'Centroid',...
-    %'Orientation', 'MajorAxisLength', 'MinorAxisLength','FilledArea',...
-    %'BoundingBox');
-    %length_pixel = tabledatarotated.BoundingBox(1,4);
-    %width_pixel = tabledatarotated.BoundingBox(1,3);
-    %area_pixel = LeafBwStats.Area;
-    %visualization
-    %rotateBWFig = figure('name','Analysis 2 of Selected Image');
-    %imshow(rotatedLeafBw);
-    %hold on, title('Rotated leaf BW with curves highlighted');
-    %plot(imgca,LeafBwStats(1).Centroid(:,1),...
-    %LeafBwStats(1).Centroid(:,2),'r*');
-    %line(RCMX,RCMY);
-    %line(RCNX,RCNY);
-    %set(impixelinfo,'Position',[5, 1 300 20]);
-    %plot(outlineLeaf(:,2),outlineLeaf(:,1),'r--','LineWidth',2.5);
-    %plots the outline 
-    %subplot(1,2,2),imshow(croppedscale,'InitialMagnification',100);
--- 1_leaf_segmentation_measurement/linear_leaf_len.m
+++
@@ -1,3 +0,0 @@
-function MaxLen=linear_leaf_len(bw)
-	bw = imfill(bw,'holes');
-	sk_bw = skeleton(bw)>20;
